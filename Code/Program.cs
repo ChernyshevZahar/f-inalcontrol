6a4ef75e022cbfda7abd7f06b10445128997bc7f
Lusi <chernyshevzahar@gmail.com>
2023-04-28 01:09:36 +0300

    Добавил метод трехзначных чисел. Обновил метод PrintArray

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.Write("Укажите размер массива ");
 int ArraySize = int.Parse(Console.ReadLine());
 
-PrintArray(UserInput(ArraySize));
+string[] strarray = UserInput(ArraySize);
+PrintArray(strarray);
+Console.WriteLine();
+PrintArray(SearchThreeDigitNumbers(strarray));
 
 string[] UserInput(int num){
     string[] arr = new string[num];
@@ -14,8 +17,34 @@ string[] UserInput(int num){
 }
 
 void PrintArray(string[] arr){
+    Console.Write($"[");
     for (int i = 0; i < arr.Length; i++)
-    {
-        Console.WriteLine(arr[i]);
+    {   
+        if(arr.Length!=i+1){
+            Console.Write($" \"{arr[i]}\",");
+        }else{
+            Console.Write($" \"{arr[i]}\"");
+        }
+        
     }
+    Console.Write($"]");
 }
+
+
+string[] SearchThreeDigitNumbers (string[] array){
+    string[] arraystr = new string[]{};
+    int num= 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length<=3){
+            Array.Resize(ref arraystr, num);
+            arraystr[num-1] = array[i];
+            num++;
+        }
+        
+    }
+    return arraystr;
+}
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
