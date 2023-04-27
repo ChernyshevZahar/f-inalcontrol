f8961de20e8b783e78e53249b05fd23c458620da
Lusi <chernyshevzahar@gmail.com>
2023-04-27 23:43:43 +0300

    Создал метод для ввода переменных от юзера

---
+++ Code/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Укажите размер массива ");
+int ArraySize = int.Parse(Console.ReadLine());
+
+UserInput(ArraySize);
+
+string[] UserInput(int num){
+    string[] arr = new string[num];
+    for (int i = 0; i < num; i++)
+    {   
+        Console.Write($"Укажите {i+1} элемент массива: ");
+        arr[i] = Console.ReadLine();
+        Console.WriteLine(arr[i]);
+    }
+    return arr;
+}
